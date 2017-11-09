2a216138ff45a5a3b0fd5fc4299adb81caff5653
Jamie Olsen <jolsendev@users.noreply.github.com>
2017-11-09 16:12:19 -0700

    Update web.py

--- web.py
+++ web.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-  return "Hello, World! I made a change!"
+  return "Hello, Anne! You are on the World Wide Web! You're a star!"
 if __name__ == "__main__":
   app.run()
