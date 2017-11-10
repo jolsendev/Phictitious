1e41523f5635aacee576a72b1b8b117d94d91a86
jolsendev <jolsendev@gmail.com>
2017-11-10 13:36:55 -0800

    added routes

--- PhictitiousBand/web.py
+++ PhictitiousBand/web.py
@@ -1,7 +1,47 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
-  return "Hello, Anne! You are on the World Wide Web! You're a star!"
+    return "Index page"
+
+
+@app.route('/login')
+def login():
+    return "Login page"
+
+
+@app.route('/setlists')
+def setlists():
+    return "Setlist page"
+
+
+@app.route('/forum')
+def forum():
+    return "Forum page"
+
+
+@app.route('/tour_dates')
+def tour_dates():
+    return "Tour date page"
+
+
+@app.route('/store')
+def store():
+    return "Store page"
+
+
+@app.route('/media')
+def media():
+    return "Media page"
+
+
+@app.route('/about')
+def about():
+    return "About page"
+
+
 if __name__ == "__main__":
-  app.run()
+    app.run()
