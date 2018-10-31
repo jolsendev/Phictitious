38f872105d413ba061d9b2a01d7cdbb8ec9927cc
a5w5nzz <jwolsen@mmm.com>
2018-10-31 12:51:53 -0600

    updated some sqlalchemy - still not working

--- PhictitiousBand/__init__.py
+++ PhictitiousBand/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, current_app
 from PhictitiousBand.api.routes import mod as api_mod
 from PhictitiousBand.site.home.routes import mod as home_mod
 from PhictitiousBand.site.merchandise.routes import mod as merch_mod
@@ -7,14 +7,20 @@ from PhictitiousBand.admin.routes import mod as admin_mod
 
 app = Flask(__name__)
 
+# Create database resources
+from PhictitiousBand.admin.models import db as admin_db
+current_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/C/TEMP/FlaskDB/test.db'
+current_app.config['SECRET_KEY'] = 'mysecret'
+
+# Admin app database
+admin_db.init_app(app)
+with app.app_context():
+    admin_db.create_all()
+
 app.register_blueprint(home_mod)
 app.register_blueprint(api_mod, url_prefix="/api")
 app.register_blueprint(merch_mod, url_prefix='/merchandise')
 app.register_blueprint(forum_mod, url_prefix='/forum')
-
-# Database config for application
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/Admin.db'
-app.config['SECRET_KEY'] = 'mysecret'
 
 # Admin App for application
 app.register_blueprint(admin_mod, url_prefix="/admin")
